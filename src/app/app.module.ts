c12b28a01b417a01ec561cb27a2180f164297dd8
Michele <michele@officinetelematiche.com>
2020-04-30 15:53:36 +0200

    Added bootstrap and made side bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideMenuComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule
